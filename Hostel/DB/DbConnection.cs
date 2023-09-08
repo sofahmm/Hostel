d6b4c46f13dc3888a792929becb5c8fbaf03a1f5
Sofia <ASUS@LAPTOP-DSSEENGE>
2023-09-08 13:08:40 +0300

    Добавьте файлы проекта.

---
+++ Hostel/DB/DbConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hostel.DB
+{
+    public class DbConnection
+    {
+        public static prog320Entities prog320Entities = new prog320Entities();
+        
+    }
+}
---
+++ Hostel/Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Hostel.DB;
+
+namespace Hostel.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public static List<Employee> employees { get; set; }
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string login = loginTb.Text.Trim();
+            string password = passwordTb.Password.Trim();
+
+            employees = new List<Employee>(DbConnection.prog320Entities.Employee.ToList());
+            Employee currentUser = employees.FirstOrDefault(i => i.Login == login && i.Password == password);
+            if (currentUser != null)
+                MessageBox.Show("Все верно");
+            else
+                MessageBox.Show("Все !верно");
+        }
+    }
+}
