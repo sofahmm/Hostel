60bf2f285ddf4caaa1c4c303774860d4e1890f60
Sofia <ASUS@LAPTOP-DSSEENGE>
2023-09-21 10:29:48 +0300

    итог

--- Hostel/Pages/MainMenuPage.xaml.cs
+++ Hostel/Pages/MainMenuPage.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Hostel.DB;
+using Hostel.Windows;
 
 namespace Hostel.Pages
 {
@@ -37,28 +38,54 @@ namespace Hostel.Pages
 
         private void AddBtn_Click(object sender, RoutedEventArgs e)
         {
-            //Client client = new Client();
+           
 
-
-            if(MessageBox.Show("Вы уверены?", "Предупреждение", MessageBoxButton.YesNoCancel) == MessageBoxResult.Yes)
-            {
-                NavigationService.Navigate(new TicketPage());
-            }
+            var dcvd = FirtsNameTb.Text + " " + NameTb.Text + " " + PatronymicTb.Text
+                + " " + BdayDp.SelectedDate + " " + PassportTb.Text + " " + PhoneTb.Text;
 
             
 
-            //client.FirstName = FirtsNameTb.Text.Trim();
-            //client.lastname = NameTb.Text.Trim();
-            //client.Patronymic = PatronymicTb.Text.Trim();
-            //client.Birthday = BdayDp.SelectedDate;
-            //client.PassportData = PassportTb.Text.Trim();
-            //client.PhoneNumber = PhoneTb.Text.Trim();
-            //client.Email = EmailTb.Text.Trim();
+            if (MessageBox.Show(dcvd, "Проверьте корректность введенных данных", MessageBoxButton.YesNo) 
+                == MessageBoxResult.Yes)
+            {
+                Client client = new Client();
 
+                client.FirstName = FirtsNameTb.Text.Trim();
+                client.lastname = NameTb.Text.Trim();
+                client.Patronymic = PatronymicTb.Text.Trim();
+                client.Birthday = BdayDp.SelectedDate;
+                client.PassportData = PassportTb.Text.Trim();
+                client.PhoneNumber = PhoneTb.Text.Trim();
+                client.Email = EmailTb.Text.Trim();
 
+                DbConnection.prog320Entities.Client.Add(client);
+                DbConnection.prog320Entities.SaveChanges();
 
-            //DbConnection.prog320Entities.Client.Add(client);
-            //DbConnection.prog320Entities.SaveChanges();
+                ClientsLv.ItemsSource = new List<Client>(DbConnection.prog320Entities.Client.ToList());
+            }
+
+            
+        }
+
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void SearchTb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ClientsLv.ItemsSource = new List<Client>(DbConnection.prog320Entities.Client.
+                Where(i => i.PassportData.StartsWith(SearchTb.Text)
+                || i.lastname.StartsWith(SearchTb.Text)).ToList());
+        }
+
+        private void ClientsLv_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Client client = 
+                ClientsLv.SelectedItem as Client;
+            TicketWindow ticketWindow = 
+                new TicketWindow(client);
+            ticketWindow.Show();    
         }
     }
 }
---
+++ Hostel/Windows/TicketWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Hostel.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hostel.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для TicketWindow.xaml
+    /// </summary>
+    public partial class TicketWindow : Window
+    {
+        public static List<Room> rooms { get; set; }
+        public static List<Class> classes { get; set; }
+
+        Client client1 = new Client();
+        public TicketWindow(Client client)
+        {
+            InitializeComponent();
+
+            client1 = client;
+            NameTb.Text = client1.lastname + " " + client1.FirstName + " " + client1.Patronymic;
+
+            rooms = new List<Room>
+                (DbConnection.prog320Entities.Room.ToList());
+            classes = new List<Class>
+                (DbConnection.prog320Entities.Class.ToList());
+            this.DataContext = this;
+        }
+
+        private void ClassesCb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Class cl = ClassesCb.SelectedItem as Class;
+            RoomsCb.ItemsSource = new List<Room>
+                (DbConnection.prog320Entities.Room.
+                Where(l => l.IdClass == cl.ID).ToList());
+        }
+    }
+}
